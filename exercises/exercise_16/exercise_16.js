77feaf438afa5ca17b4f11168fd391774d6e25f1
Max V <fullstacksmax@gmail.com>
2025-01-27 22:27:29 +0000

    ex 16 done

--- exercises/exercise_16/exercise_16.js
+++ exercises/exercise_16/exercise_16.js
@@ -10,4 +10,22 @@ Use Object.create to set the prototype of the returned object to a
 shared prototype object. 
 
 Demonstrate that multiple users share the same prototype. */
+const person = {
+    sayHello: function () {
+        return `hello my name is ${this.name}`
+    }
+}
 
+function createUser(name) {
+    const user = Object.create(person);
+    user.name = name;
+    return user;
+}
+
+const user1 = createUser("max")
+const user2 = createUser("lily")
+console.log(user1.sayHello())
+console.log(user2.sayHello())
+
+console.log(Object.getPrototypeOf(user1) === person)
+console.log(Object.getPrototypeOf(user2) === person)
